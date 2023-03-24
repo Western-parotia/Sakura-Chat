d5d48ea9ce31e5d38075173d37fca9301f14dc4c
zhushiwen <sink50@foxmail.com>
2023-03-24 18:34:52 +0800

    增加prompt 限制返回中文语音解析内容

--- app/src/main/java/com/sakura/chat/v2/base/net/GPTAPIService.kt
+++ app/src/main/java/com/sakura/chat/v2/base/net/GPTAPIService.kt
@@ -10,11 +10,13 @@ import retrofit2.http.*
 
 interface GPTAPIService {
 
+    //prompt 可帮助修改返回的文吧语言类型，未查到相关API，但是通过Bing 聊天可以快速了解
     @Multipart
     @POST("/v1/audio/translations")
     suspend fun translationsVoice(
         @Part filePart: MultipartBody.Part,
-        @Part model: MultipartBody.Part
+        @Part model: MultipartBody.Part,
+        @Part("prompt") prompt: String = "请转录以下中文语音"
     ): Response<VoiceRes>
 
     @Headers("Content-Type: application/json")
