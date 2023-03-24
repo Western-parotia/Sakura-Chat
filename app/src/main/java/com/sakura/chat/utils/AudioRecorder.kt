6be437d8c06785ed912ed921c38e0ba485a8e407
WangNeng <873456775@qq.com>
2023-03-24 17:01:35 +0800

    聊天基本代码提交

---
+++ app/src/main/java/com/sakura/chat/utils/AudioRecorder.kt
@@ -0,0 +1,66 @@
+package com.sakura.chat.utils
+
+import android.media.MediaRecorder
+import com.sakura.chat.core.App
+import java.io.File
+
+class AudioRecorder {
+    private var recorder = MediaRecorder()
+
+    /**
+     * -1停止，0暂停，1录音中，2录音完成
+     */
+    var state = -1
+        private set
+
+    val fileDir = File(App.get().cacheDir.absolutePath + "/audio").let {
+        if (!it.exists()) {
+            it.mkdirs()
+        }
+        "${it.absolutePath}/${System.currentTimeMillis()}.3gp"
+    }
+
+    init {
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC)
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+        recorder.setOutputFile(fileDir)
+        recorder.prepare()
+    }
+
+    fun start() {
+        when (state) {
+            -1 -> {
+                recorder.start()
+                state = 1
+            }
+            0 -> {
+                resume()
+            }
+        }
+    }
+
+    fun pause() {
+        if (state == 1) {
+            recorder.pause()
+            state = 0
+        }
+    }
+
+    fun resume() {
+        if (state == 0) {
+            recorder.resume()
+            state = 1
+        }
+    }
+
+    fun stop() {
+        when (state) {
+            0, 1 -> {
+                state = 2
+                recorder.stop()
+                recorder.release()
+            }
+        }
+    }
+}
