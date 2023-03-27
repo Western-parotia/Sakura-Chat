04effe2cbdb236713c9ffb16b50fd11928d99be9
zhushiwen <sink50@foxmail.com>
2023-03-27 10:37:22 +0800

    添加测试页面

---
+++ app/src/main/java/com/sakura/chat/v2/business/main/TestActivity.kt
@@ -0,0 +1,20 @@
+package com.sakura.chat.v2.business.main
+
+import android.os.Bundle
+import androidx.viewbinding.ViewBinding
+import com.sakura.chat.databinding.ActTestBinding
+import com.sakura.chat.v2.base.component.BaseActivityV2
+
+class TestActivity : BaseActivityV2() {
+    private val vb by lazyVB<ActTestBinding>()
+    override fun getContentVB(): ViewBinding? {
+        return vb
+    }
+
+    override fun bindData() {
+    }
+
+    override fun init(savedInstanceState: Bundle?) {
+    }
+
+}
